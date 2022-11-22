f7443a5cd4c63d945be3bd1777ed1a8bb4f8896f
NirmalaMani <nirmalamani16@gmail.com>
2022-11-22 13:16:41 -0700

    jenkinsfile

--- provider.tf
+++ provider.tf
@@ -12,3 +12,11 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
 }
+terraform {
+
+  backend "s3" {
+    bucket = "terraformstatebermtec"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -10,12 +10,12 @@ variable "instancetype" {
 
 variable "access_key" {
   type=string
-  default="AKIAUUZA6LO2V3MISAWV"
+  default=""
 }
 
 variable "secret_key" {
   type=string
-  default="hJVuQKZvArmP9Jwl4neNZEz4kknixHze3VKiH+5x"
+  default=""
 }
 
 variable "region" {
